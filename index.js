16d1da2f8cb98077b6912df8cc3c103672432377
kenanhancer <kenanhancer@gmail.com>
2020-04-18 02:59:49 +0100

    cli bug fix

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
 #!/usr/bin/env node
 'use strict';
 
-const nutIoc = require('nut-ioc');
-const open = require('open');
-
-const nutIocContainer = nutIoc();
-
-nutIocContainer.use({ dependencyPath: './src' });
-
-const mainAsync = async () => {
-
-    const { swaggerDefaultControllerCmd } = await nutIocContainer.build();
-
-    const argv = require('yargs')
-        .version()
-        .usage('Usage: $0 <command> [options]')
-        .command(['generate-default-controller [swagger-file] [destination-dir]', 'generate', 'g'],
-            'Generate Javascript file from swagger in destination directory',
-            swaggerDefaultControllerCmd)
-        .option('overwrite', {
-            alias: 'o',
-            description: 'Overwrite existing file',
-            type: 'boolean',
-            default: false
-        })
-        .option('isEmptyBody', {
-            alias: 'ieb',
-            description: 'Generating function body',
-            type: 'boolean',
-            default: false
-        })
-        .demandOption(['swagger-file', 'destination-dir'])
-        .example('$0 generate-js hello-world.yaml ./', 'Generate Javascript file from swagger in `my-project` directory.')
-        .command(['docs'], 'Go to the documentation at https://kenanhancer.com', {}, _ => open('https://kenanhancer.com'))
-        .help('h')
-        .alias('h', 'help')
-        .epilogue('for more information, find the documentation at https://kenanhancer.com')
-        .argv;
-};
-
-mainAsync();
+require('../nut-swagger/cli').default();
