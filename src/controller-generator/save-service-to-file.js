e7d5c148e5cc405e635a1cb422ebd9edfb66109a
kenanhancer <kenanhancer@gmail.com>
2020-04-26 12:10:09 +0100

    writing name of actions

--- src/controller-generator/save-service-to-file.js
+++ src/controller-generator/save-service-to-file.js
@@ -9,13 +9,15 @@ module.exports.Service = ({ fsAsync: { existsAsync, mkdirAsync, writeFileAsync }
             const filePath = path.join(destinationDir, `${serviceName}.js`);
 
             if (await existsAsync(filePath) && !overwrite) {
-                console.error(`${filePath} file exists and overwrite option is false. So it is not created.`);
+                console.error(`NUT-IOC ERROR: ${filePath} file exists and overwrite option is false. So it is not created.`);
             } else {
                 if (!(await existsAsync(destinationDir))) {
                     await mkdirAsync(destinationDir);
+                    console.log(`NUT-IOC INFO: Generated directory path is ${destinationDir}`);
                 }
 
                 await writeFileAsync(filePath, generatedServiceCode);
+                console.log(`NUT-IOC INFO: Writen file path is ${filePath}`);
             }
         }
     };
